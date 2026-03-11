37529e53f139413c22cc323809e080a7ea458be4
Andryushkak <kozunaandrey@gmail.com>
2026-03-11 22:40:38 +0200

    edit

--- routes/profile.js
+++ routes/profile.js
@@ -12,27 +12,39 @@ router.get('/profile/edit', (req, res) => {
 });
 
 // POST з фото
-router.post('/profile/edit', upload.single('photo'), async (req, res) => {
+router.post('/profile/edit', (req, res) => {
   if (!req.isAuthenticated()) return res.redirect('/login');
 
-  const { firstName, lastName, email } = req.body;
-
-  try {
-    const user = await database.findUserById(req.user._id);
-    user.firstName = firstName;
-    user.lastName = lastName;
-    user.email = email;
-
-    if (req.file) {
-      user.photo = req.file.path; // Cloudinary URL
+  upload.single('photo')(req, res, async (err) => {
+    if (err) {
+      console.error('Помилка multer/cloudinary:', err);
+      return res.status(500).send(`Помилка завантаження фото: ${err.message}`);
     }
 
-    await user.save();
-    res.redirect('/profile');
-  } catch (err) {
-    console.error('Помилка оновлення профілю:', err);
-    res.status(500).send('Помилка сервера');
-  }
+    const { firstName, lastName, email } = req.body;
+
+    try {
+      const user = await database.findUserById(req.user._id);
+
+      if (!user) {
+        return res.status(404).send('Користувача не знайдено');
+      }
+
+      user.firstName = firstName;
+      user.lastName = lastName;
+      user.email = email;
+
+      if (req.file) {
+        user.photo = req.file.path;
+      }
+
+      await user.save();
+      res.redirect('/profile');
+    } catch (err) {
+      console.error('Помилка оновлення профілю:', err);
+      res.status(500).send(`Помилка сервера: ${err.message}`);
+    }
+  });
 });
 
 module.exports = router;
